7624a3c2289a05a2efb9531435862f50d1ba9b3c
Trevor Roberts Jr <trevorr@amazon.com>
2019-12-30 22:25:36 -0500

    improve ball position check logic

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -29,12 +29,6 @@ void process_image_callback(const sensor_msgs::Image img)
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
 
-
-    // ROS_INFO(img.encoding);
-    // ROS_INFO("%1.2f %1.2f %1.2f",(float)img.data[0],(float)img.data[1],(float)img.data[2]);
-    // ROS_INFO("Number of Columns:%1.2f Number of Rows:%1.2f Number of Pixels?:%1.2f Array Size:%1.2f",(float)img.width,(float)img.height,(float)img.width*img.height,(float)img.data.size());
-    // ROS_INFO("Step:%1.2f",(float)img.step);
-    // ROS_INFO("height: %1.2f width: %1.2f",(float)img.height,(float)img.width );
     int height_start = 0;
     int height_end = img.height;
     int left_start = 0;
@@ -46,43 +40,44 @@ void process_image_callback(const sensor_msgs::Image img)
     bool is_left = false;
     bool is_center = false;
     bool is_right = false;
-    // uint8_t pixels = img.data;
-    
 
-    // bool found = false;
-    // int found_width = 0;
-    // int pixel_count=0;
-
+// Search in the left third of the screen
     for (int i = left_start; i < left_end; i++) {
         if (img.data[i] == 255 && img.data[i+1] == 255 && img.data[i+2] == 255) {
             is_left = true;
+            ROS_INFO("White Left Found");
             break;
         }
     }
+// Search in the center third of the screen
     for (int i = center_start; i < center_end; i++) {
         if (img.data[i] == 255 && img.data[i+1] == 255 && img.data[i+2] == 255) {
             is_center = true;
+            ROS_INFO("White Center Found");
             break;
         }
     }
+// Search in the right third of the screen
     for (int i = right_start; i < right_end; i++) {
         if (img.data[i] == 255 && img.data[i+1] == 255 && img.data[i+2] == 255) {
             is_right = true;
+            ROS_INFO("White Right Found");
             break;
         }
     }
     
-    if (is_left&&is_center&&is_right) {
-        ROS_INFO("Hi Ho, Silver!");
-    } else if (is_left) {
+    if ((is_left&&is_center&&is_right) || (!is_left&&is_center&&!is_right)) {
+        ROS_INFO("Drive Forward");
+    }
+    if (is_left&&!is_right) {
         ROS_INFO("Left Turn!");
-    } else if (is_right) {
+    }
+    if (is_right&&!is_left) {
         ROS_INFO("Right Turn!");
-    } else {
+    }
+    if (!is_left&&!is_center&&!is_right) {
         ROS_INFO("Full Stop!");
     }
-
-    // ROS_INFO("Number of Pixels?:%1.2f",(float)pixel_count);
 }
 
 int main(int argc, char** argv)
